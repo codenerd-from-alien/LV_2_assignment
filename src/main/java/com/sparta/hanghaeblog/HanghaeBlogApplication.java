e14fe9255b41c835583a89293e7c06abca31f3dd
Seoyoon <wideskyinme@gmail.com>
2023-06-25 19:05:26 +0900

    Initial commit

---
+++ src/main/java/com/sparta/hanghaeblog/HanghaeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeBlogApplication.class, args);
+	}
+
+}
