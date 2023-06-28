edaac29cbe621229e5e5266aad6ef185b21f6dc7
Seoyoon <wideskyinme@gmail.com>
2023-06-28 16:13:43 +0900

    리팩토링 1(RequiredArgsConstructor, sout제거)

--- src/main/java/com/sparta/hanghaeblog/controller/PostController.java
+++ src/main/java/com/sparta/hanghaeblog/controller/PostController.java
@@ -5,18 +5,20 @@ import com.sparta.hanghaeblog.dto.SuccessResponse;
 import com.sparta.hanghaeblog.service.PostService;
 import com.sparta.hanghaeblog.dto.PostRequestDto;
 import com.sparta.hanghaeblog.dto.PostResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor //final로 선언된 애들에 대해서 생성자를 직접 만들어준다.
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api")//posts도 공통이니 posts도 추가할것
 public class PostController {
-    private final PostService postService;
+    private final PostService postService; //무조건 생성자를 생성해주거나 바로 초기화해줘야함 & 객체의 주소값이 불변 //왜 이렇게 할까?
 
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
+//    public PostController(PostService postService) { //생성자, DI(의존성 주입)의 역할을 한다.
+//        this.postService = postService;
+//    } -> @RequiredArgsConstructor로 정리될 수 있음
 
     // 글 목록 조회
     @GetMapping("/posts")
@@ -26,7 +28,6 @@ public class PostController {
 
     @GetMapping("/posts/contents")
     public List<PostResponseDto> getPostsByKeyword(@RequestParam String keyword){
-        System.out.println("컨트롤러입니다");
         return postService.getPostsByKeyword(keyword);
     }
 
