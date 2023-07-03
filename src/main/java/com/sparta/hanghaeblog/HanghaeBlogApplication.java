a432d3f17d3535cc87d7eb13905940d57e59390f
Seoyoon <wideskyinme@gmail.com>
2023-07-03 17:38:38 +0900

    properties추가

--- src/main/java/com/sparta/hanghaeblog/HanghaeBlogApplication.java
+++ src/main/java/com/sparta/hanghaeblog/HanghaeBlogApplication.java
@@ -3,8 +3,9 @@ package com.sparta.hanghaeblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class HanghaeBlogApplication {
 
